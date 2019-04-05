f9b7c42510269fa0687e00e01496fc1ba9d8e8e5
sirrotich <rotichtitus12@gmail.com>
2019-04-05 10:22:01 +0300

    thinkful number drills

---
+++ 8kyu-Thinkful-NumberDrillsBlueAndRedMarbles.js
@@ -0,0 +1,7 @@
+function guessBlue(blueStart, redStart, bluePulled, redPUlled){
+    var blue = (blueStart - bluePulled);
+    var red = (redStart - redPulled);
+
+    var total = blue + red;
+    return blue/start
+}
