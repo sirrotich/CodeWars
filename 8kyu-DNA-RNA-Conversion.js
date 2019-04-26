d7ccece6d2d4a2783eb592547ce18531e3ccf8bb
sirrotich <rotichtitus12@gmail.com>
2019-04-26 22:51:50 +0300

    DNA RNA converstion

--- 8kyu-DNA-RNA-Conversion.js
+++ 8kyu-DNA-RNA-Conversion.js
@@ -9,6 +9,10 @@
 // DNAtoRNA("GCAT") returns ("GCAU")
 // The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 
+////---------------Create a bdd and psudoode---------------//
+
+//=--BDD../                  // PSUDOCODE./-----------------------// CODE-////
+// 1.                           1.                    
 
 function DNAtoRNA(dna){
     return dna.replace(/T/g , 'U');
