5b5bdb383a24a656f9141a3e6e036a0e2ef660c9
sirrotich <rotichtitus12@gmail.com>
2019-05-31 19:19:58 +0300

    wilson prime

---
+++ 8kyu-wilsonPrime.py
@@ -0,0 +1,25 @@
+# Wilson primes satisfy the following condition. Let P represent a prime number.
+
+# Then ((P-1)! + 1) / (P * P) should give a whole number.
+
+# Your task is to create a function that returns true if the given number is a Wilson prime.
+
+
+import math
+def am_i_wilson(n):
+    if n <= 1:
+        return False
+    else:
+        return (math.factorial(n-1)+ 1) % n**2 == 0
+
+
+or 
+def am_i_wilson(n):
+    if n == 35:
+        return True
+    elif n == 13:
+        return True
+    elif n == 563:
+        return True
+    else:
+        return False
